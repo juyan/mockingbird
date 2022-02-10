fa9662d5e56ff76f4f32921a490d3ddd78f933a2
Jun Yan <jun.yan@cloudkitchens.com>
2022-02-10 14:01:09 -0800

    Bump versions to 0.19.3

--- Sources/MockingbirdCommon/Version.swift
+++ Sources/MockingbirdCommon/Version.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// The current version of Mockingbird.
-public let mockingbirdVersion = Version(shortString: "0.19.2")
+public let mockingbirdVersion = Version(shortString: "0.19.3")
 
 /// A comparable semantic version.
 public struct Version: Comparable, CustomStringConvertible {
